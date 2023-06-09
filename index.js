f52ac80378298f96d882ed65e8600daca82f6e84
Sunitha R <125405578+SunithaSunitha@users.noreply.github.com>
2023-06-09 04:16:05 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,107 @@
+const express = require('express');
 const  mongoose  = require('mongoose');
-const cities = require('./cities')
-const {places, descriptors} = require('./seedHelpers')
-const Campground = require('../models/campground');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const catchAsync= require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
+const Campground = require('./models/campground');
+const session = require('express-session');
+const Review = require('./models/review');
+const flash= require('connect-flash')
+const passport= require('passport')
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+
+
+const {campgroundSchema,reviewSchema} = require('./schemas.js')
+const methodOverride = require('method-override');
+
+const campgrounds= require('./routes/campgrounds');
+const reviews= require('./routes/reviews');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     .then(()=>{
         console.log('Mongo connection open')
     })
     .catch(err=>{
-    console.log("oh no Mongo connection error!")
+    console.log("oh no Mongo connection e rror!")
     console.log(err)
 })
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
 
-const seedDB = async ()=>{
-    await Campground.deleteMany({});
-    for(let i=0; i<50; i++){
-        const random1000 = Math.floor(Math.random()*1000);
-        const price = Math.floor(Math.random()*20)+10;
-        const camp = new Campground({
-            title: `${sample(descriptors)} ${sample(places)}`,
-            location:`${cities[random1000].city}, ${cities[random1000].state}`,
-            image:'https://source.unsplash.com/collection/483251',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sapiente est, quisquam quia at cupiditate veritatis animi, minima, voluptates ipsam illo libero voluptate debitis ipsum laborum? Alias nisi eius ducimus?',
-            price 
-        })
-        await camp.save(); 
+const app = express();
+
+app.engine('ejs',ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'))
+//app.use(express.static('public'))
+app.use(express.static(path.join(__dirname,'public')))
+
+const sessionConfig={
+    secret:'thisshouldbebettersecret!',
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        httpOnly:"true",
+        expires:Date.now()+ 1000*60*60*24*7,
+        maxAge:1000*60*60*24*7
     }
-    // const c = new Campground({title: 'purple field'});
-    // await c.save();
-};
+}
+app.use(session(sessionConfig))
+app.use(flash());
 
-seedDB().then(() => {
-    mongoose.connection.close();
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req,res,next)=>{
+    res.locals.success=req.flash('success');
+    res.locals.error=req.flash('error');
+    next();
 })
+
+app.get('/fakeUser', async (req,res) =>{
+    const user = new User ({email:'coltttt@gmail.com',username:'colttt'})
+    const newUser = await User.register(user,'chicken')
+    res.send(newUser);
+} )
+
+app.use('/campgrounds',campgrounds)
+app.use('/campgrounds/:id/reviews',reviews)
+
+app.get('/', (req, res)=>{
+    res.render('home')
+})
+
+
+// app.get('/makecampground', async (req, res)=>{
+//     const camp = new Campground({title:'My Backyard', description : 'cheap camping!'});
+//     await camp.save();
+//     res.send(camp)
+// })
+
+
+app.all('*',(req,res,next)=>{
+    next(new ExpressError('Page Not Found',404))
+    // res.send("404!!")
+})
+
+app.use((err,req,res,next) => {
+    const{statusCode=500} = err;
+    if(!err.message) err.message = 'Oh No,something wrong'
+    res.status(statusCode).render('error',{err})
+    // res.send('Oh boy,Something went wrong!')
+})
+
+app.listen(3000, ()=>{
+    console.log('Serving on port 3000')
+})
+
+//Post /campground/:id/review
